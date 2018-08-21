40189b3ce73d075540588e241a0b8871b2b41ee8
Vlastimil Dolejš <vlastimil.dolejs@diribet.cz>
2018-08-21 15:55:01 +0200

    reverted to logic from old set_positions.js file

--- src/traces/box-diribet/cross_trace_calc.js
+++ src/traces/box-diribet/cross_trace_calc.js
@@ -1,114 +1,90 @@
 /**
-* Copyright 2012-2018, Plotly, Inc.
-* All rights reserved.
-*
-* This source code is licensed under the MIT license found in the
-* LICENSE file in the root directory of this source tree.
-*/
+ * Copyright 2012-2016, Plotly, Inc.
+ * All rights reserved.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
 
 'use strict';
 
+var Registry = require('../../registry');
 var Axes = require('../../plots/cartesian/axes');
 var Lib = require('../../lib');
 
-var orientations = ['v', 'h'];
 
 function crossTraceCalc(gd, plotinfo) {
-    var calcdata = gd.calcdata;
-    var xa = plotinfo.xaxis;
-    var ya = plotinfo.yaxis;
+	var fullLayout = gd._fullLayout,
+		calcdata = gd.calcdata,
+		xa = plotinfo.xaxis,
+		ya = plotinfo.yaxis,
+		orientations = ['v', 'h'];
+	var posAxis, i, j, k;
 
-    for(var i = 0; i < orientations.length; i++) {
-        var orientation = orientations[i];
-        var posAxis = orientation === 'h' ? ya : xa;
-        var boxList = [];
-        var minPad = 0;
-        var maxPad = 0;
+	for(i = 0; i < orientations.length; ++i) {
+		var orientation = orientations[i],
+			boxlist = [],
+			boxpointlist = [],
+			cd,
+			t,
+			trace;
 
-        // make list of boxes / candlesticks
-        // For backward compatibility, candlesticks are treated as if they *are* box traces here
-        for(var j = 0; j < calcdata.length; j++) {
-            var cd = calcdata[j];
-            var t = cd[0].t;
-            var trace = cd[0].trace;
+		// set axis via orientation
+		if(orientation === 'h') posAxis = ya;
+		else posAxis = xa;
 
-            if(trace.visible === true &&
-                    (trace.type === 'box' || trace.type === 'candlestick') &&
-                    !t.empty &&
-                    (trace.orientation || 'v') === orientation &&
-                    trace.xaxis === xa._id &&
-                    trace.yaxis === ya._id
-              ) {
-                boxList.push(j);
+		// make list of box traces
+		for(j = 0; j < calcdata.length; ++j) {
+			cd = calcdata[j];
+			t = cd[0].t;
+			trace = cd[0].trace;
 
-                if(trace.boxpoints) {
-                    minPad = Math.max(minPad, trace.jitter - trace.pointpos - 1);
-                    maxPad = Math.max(maxPad, trace.jitter + trace.pointpos - 1);
-                }
-            }
-        }
+			if(trace.visible === true && Registry.traceIs(trace, 'box-violin') &&
+				!t.emptybox &&
+				trace.orientation === orientation &&
+				trace.xaxis === xa._id &&
+				trace.yaxis === ya._id) {
+				boxlist.push(j);
+			}
+		}
 
-        setPositionOffset('box', gd, boxList, posAxis, [minPad, maxPad]);
-    }
-}
+		// make list of box points (boxes)
+		for(j = 0; j < boxlist.length; j++) {
+			cd = calcdata[boxlist[j]];
+			for(k = 0; k < cd.length; k++) boxpointlist.push(cd[k].pos);
+		}
+		if(!boxpointlist.length) continue;
 
-function setPositionOffset(traceType, gd, boxList, posAxis, pad) {
-    var calcdata = gd.calcdata;
-    var fullLayout = gd._fullLayout;
-    var pointList = [];
+		// box plots - update dPos based on multiple traces
+		// and then use for posAxis autorange
 
-    // N.B. reused in violin
-    var numKey = traceType === 'violin' ? '_numViolins' : '_numBoxes';
+		var boxdv = Lib.distinctVals(boxpointlist),
+			dPos = boxdv.minDiff / 2;
 
-    var i, j, calcTrace;
+		// if there's no duplication of x points,
+		// disable 'group' mode by setting numboxes=1
+		if(boxpointlist.length === boxdv.vals.length) fullLayout._numBoxes = 1;
 
-    // make list of box points
-    for(i = 0; i < boxList.length; i++) {
-        calcTrace = calcdata[boxList[i]];
-        for(j = 0; j < calcTrace.length; j++) {
-            pointList.push(calcTrace[j].pos);
-        }
-    }
+		// check for forced minimum dtick
+		Axes.minDtick(posAxis, boxdv.minDiff, boxdv.vals[0], true);
 
-    if(!pointList.length) return;
+		// autoscale the x axis
+		var extremes = Axes.findExtremes(posAxis, boxdv.vals, {
+			vpadminus: dPos,
+			vpadplus: dPos
+		});
 
-    // box plots - update dPos based on multiple traces
-    // and then use for posAxis autorange
-    var boxdv = Lib.distinctVals(pointList);
-    var dPos = boxdv.minDiff / 2;
+		for(i = 0; i < boxlist.length; i++) {
+			cd = calcdata[boxlist[i]];
+			// set the width of all boxes
+			cd[0].t.dPos = dPos;
+			// link extremes to all boxes
+			cd[0].trace._extremes[posAxis._id] = extremes;
+		}
 
-    // if there's no duplication of x points,
-    // disable 'group' mode by setting counter to 1
-    if(pointList.length === boxdv.vals.length) {
-        fullLayout[numKey] = 1;
-    }
-
-    // check for forced minimum dtick
-    Axes.minDtick(posAxis, boxdv.minDiff, boxdv.vals[0], true);
-
-    var gap = fullLayout[traceType + 'gap'];
-    var groupgap = fullLayout[traceType + 'groupgap'];
-    var padfactor = (1 - gap) * (1 - groupgap) * dPos / fullLayout[numKey];
-
-    // autoscale the x axis - including space for points if they're off the side
-    // TODO: this will overdo it if the outermost boxes don't have
-    // their points as far out as the other boxes
-    var extremes = Axes.findExtremes(posAxis, boxdv.vals, {
-        vpadminus: dPos + pad[0] * padfactor,
-        vpadplus: dPos + pad[1] * padfactor
-    });
-
-    for(i = 0; i < boxList.length; i++) {
-        calcTrace = calcdata[boxList[i]];
-        // set the width of all boxes
-        calcTrace[0].t.dPos = dPos;
-        // link extremes to all boxes
-        calcTrace[0].trace._extremes[posAxis._id] = extremes;
-    }
-
-}
+	}
+};
 
 module.exports = {
-    crossTraceCalc: crossTraceCalc,
-    setPositionOffset: setPositionOffset
+	crossTraceCalc: crossTraceCalc
 };
